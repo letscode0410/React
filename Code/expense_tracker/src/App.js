d26dab34c39418e17aa530ce70c577348a849853
Ravi Kumar <ravikumar.p536@gmail.com>
2023-05-12 22:50:59 +0530

    custom  component

--- Code/expense_tracker/src/App.js
+++ Code/expense_tracker/src/App.js
@@ -1,13 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import ExpenseItem from './components/ExpenseItem';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-       
-          Learn React
-      </header>
+      <ExpenseItem/>
     </div>
   );
 }
