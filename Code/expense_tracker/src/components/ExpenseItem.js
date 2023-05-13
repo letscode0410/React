b6413801848e0fa52e5af08854f8d1a6c9cfbe39
Ravi Kumar <ravikumar.p536@gmail.com>
2023-05-13 12:04:34 +0530

    splitting component into multiple components

--- Code/expense_tracker/src/components/ExpenseItem.js
+++ Code/expense_tracker/src/components/ExpenseItem.js
@@ -1,4 +1,5 @@
 import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
 
 function ExpenseItem(props) {
 
@@ -7,11 +8,7 @@ function ExpenseItem(props) {
   const year = props.date.getFullYear();
   return (
     <div className="expense-item">
-      <div>
-        <div>{month}</div>
-        <div>{day}</div>
-        <div>{year}</div>
-        </div>
+      <ExpenseDate date={props.date}/>
       <div className="expense-item__description">
         <h2>{props.name}</h2>
         <div className="expense-item__price">${props.amount}</div>
