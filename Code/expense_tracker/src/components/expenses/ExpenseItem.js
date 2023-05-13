42a3319b8404b1043346f5091ef6dd4900d2449e
Ravi Kumar <ravikumar.p536@gmail.com>
2023-05-13 12:58:56 +0530

    rearranged components into sub folders

--- Code/expense_tracker/src/components/expenses/ExpenseItem.js
+++ Code/expense_tracker/src/components/expenses/ExpenseItem.js
@@ -1,12 +1,9 @@
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
-import Card from "./Card"
+import Card from "../UI/Card"
 
 function ExpenseItem(props) {
 
-  const month = props.date.toLocaleDateString('en-US',{month:'long'});
-  const day = props.date.toLocaleDateString('en-US',{day:'2-digit'});
-  const year = props.date.getFullYear();
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date}/>
