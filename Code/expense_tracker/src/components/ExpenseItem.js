5fa012fcaaabc19f92950ad1e129fdd7c086c2fa
Ravi Kumar <ravikumar.p536@gmail.com>
2023-05-13 10:54:57 +0530

    Updated ExpenseItem with Expressions

--- Code/expense_tracker/src/components/ExpenseItem.js
+++ Code/expense_tracker/src/components/ExpenseItem.js
@@ -1,12 +1,16 @@
 import "./ExpenseItem.css";
 
 function ExpenseItem() {
+
+  const expenseDate=new Date(2023,4,10);
+  const expenseName ="Car Insuarnce";
+  const expensePrice = 294.67;
   return (
     <div className="expense-item">
-      <div>March 28th 2021</div>
+      <div>{expenseDate.toLocaleDateString()}</div>
       <div className="expense-item__description">
-        <h2>Car Insurance</h2>
-        <div className="expense-item__price">$294.67</div>
+        <h2>{expenseName}</h2>
+        <div className="expense-item__price">${expensePrice}</div>
       </div>
     </div>
   );
