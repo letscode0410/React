63fdc42fb2c1f9a0fc223329ee668590322219ed
Ravi Kumar <ravikumar.p536@gmail.com>
2023-05-13 11:53:37 +0530

    added normal javascript logic into component

--- Code/expense_tracker/src/components/ExpenseItem.js
+++ Code/expense_tracker/src/components/ExpenseItem.js
@@ -2,9 +2,16 @@ import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
 
+  const month = props.date.toLocaleDateString('en-US',{month:'long'});
+  const day = props.date.toLocaleDateString('en-US',{day:'2-digit'});
+  const year = props.date.getFullYear();
   return (
     <div className="expense-item">
-      <div>{props.date.toLocaleDateString()}</div>
+      <div>
+        <div>{month}</div>
+        <div>{day}</div>
+        <div>{year}</div>
+        </div>
       <div className="expense-item__description">
         <h2>{props.name}</h2>
         <div className="expense-item__price">${props.amount}</div>
