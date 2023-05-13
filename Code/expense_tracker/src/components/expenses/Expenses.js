31cd1a7923244babbd850a0766566d599848cfa8
Ravi Kumar <ravikumar.p536@gmail.com>
2023-05-13 14:39:51 +0530

    update expenses.js

--- Code/expense_tracker/src/components/expenses/Expenses.js
+++ Code/expense_tracker/src/components/expenses/Expenses.js
@@ -6,22 +6,22 @@ function Expenses(props) {
   return (
     <Card className="expenses">
       <ExpenseItem
-        title={props.items[0].title}
+        name={props.items[0].title}
         amount={props.items[0].amount}
         date={props.items[0].date}
       />
       <ExpenseItem
-        title={props.items[1].title}
+        name={props.items[1].title}
         amount={props.items[1].amount}
         date={props.items[1].date}
       />
       <ExpenseItem
-        title={props.items[2].title}
+        name={props.items[2].title}
         amount={props.items[2].amount}
         date={props.items[2].date}
       />
       <ExpenseItem
-        title={props.items[3].title}
+        name={props.items[3].title}
         amount={props.items[3].amount}
         date={props.items[3].date}
       />
