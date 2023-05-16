a006bbada7fdfe01dc37bbe34bc82ed536e294b3
Ravi Kumar <ravikumar.p536@gmail.com>
2023-05-16 12:44:03 +0000

    handled input expense data with state

--- Code/expense_tracker/src/components/newExpense/ExpenseForm.js
+++ Code/expense_tracker/src/components/newExpense/ExpenseForm.js
@@ -1,20 +1,48 @@
 import "./ExpenseForm.css";
+import { useState } from 'react'
 
 const ExpenseForm = () => {
+
+  const [name, setName] = useState('');
+  const [price, setPrice] = useState('');
+  const [date, setDate] = useState('');
+
+  const onNameChangeHander = (event) => {
+    console.log(event);
+    setName(event.target.value);
+  };
+  const onPriceChangeHander = (event) => {
+    setPrice(event.target.value);
+  };
+  const onDateChangeHander = (event) => {
+    setDate(event.target.value);
+  };
+  const onExpenseSubmit = (event) => {
+    event.preventDefault();
+    const expenseData = {
+      name: name,
+      price: price,
+      date: new Date(date)
+    }
+    console.log(expenseData);
+    setName(' ');
+    setPrice(' ');
+    setDate(' ');
+  }
   return (
-    <form>
+    <form onSubmit={onExpenseSubmit}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Name</label>
-          <input type="text" />
+          <input type="text" value={name} onChange={onNameChangeHander} />
         </div>
         <div className="new-expense__control">
           <label>Price</label>
-          <input type="number" min="0.01" step="0.01" />
+          <input type="number" value={price} min="0.01" step="0.01" onChange={onPriceChangeHander} />
         </div>
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" min="2019-01-01" max="2023-12-31" />
+          <input type="date" value={date} min="2019-01-01" max="2023-12-31" onChange={onDateChangeHander} />
         </div>
       </div>
       <div className="new-expense__actions">
